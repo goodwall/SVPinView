0debc1fb79419f8e08bd234c92cfba3d8051b373
caotrido <caotrido@users.noreply.github.com>
2021-11-19 09:01:58 +0100

    Removed insets

--- SVPinView/Source/Classes/SVPinView.swift
+++ SVPinView/Source/Classes/SVPinView.swift
@@ -397,7 +397,7 @@ extension SVPinView : UICollectionViewDataSource, UICollectionViewDelegate, UICo
             let totalCellWidth = height * CGFloat(pinLength)
             let totalSpacingWidth = interSpace * CGFloat(max(pinLength, 1) - 1)
             let inset = (collectionView.frame.size.width - CGFloat(totalCellWidth + CGFloat(totalSpacingWidth))) / 2
-            return UIEdgeInsets(top: top, left: inset, bottom: 0, right: inset)
+            return UIEdgeInsets(top: top, left: 0, bottom: 0, right: 0)
         }
         return UIEdgeInsets(top: top, left: 0, bottom: 0, right: 0)
     }
